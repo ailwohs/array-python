82affdc342c5a0ee857b54c9f7908d6d5dddad34
julha leithold <juliaavancisoares@gmail.com>
2023-05-12 16:58:19 -0300

    array em python aula inicial

---
+++ aula1.py
@@ -0,0 +1,41 @@
+'''from random import menu
+
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+print(x)
+x.pop(0)
+print(x)
+#Pop remove o indicie - remove mesmo se nao tiver
+
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+print(x)
+x.remove(5)
+print(x)
+#Remove ira remover o elemento - so ira remover se existir o elemento solicitado 
+
+x=[1,2,3,4,5,6]
+print(x)
+x.append(13)
+print(x)
+x.append('igreja')
+print(x)
+#append serve para acrescentar elementos 
+
+x=[]
+print(x)
+x.append(1)
+x.append(2)
+x.append(3)
+x.append(4)
+x.append(5)
+print(x)
+#elemento vazio para ascrecentar elementos com o append '''
+
+x=[1,2,4,5,7]
+print(x)
+x.insert(3,6)
+print(x)
+#insert serve para adicionar um indicie
+x=['a','i','o','u']
+print(x)
+x.insert(1,'e')
+print(x)
