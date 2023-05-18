e62a4c0a2897ad45a1c90acfe6fb2485549462ba
julha leithold <juliaavancisoares@gmail.com>
2023-05-18 17:06:35 -0300

    atividade 2 - erros de formatação por conta do vscode - estão corretas

---
+++ atividade2.py
@@ -0,0 +1,117 @@
+'''if else elif'''
+
+ dia=input("Digite um número de 1 até 7 \n>")
+  if dia==1:
+      print("Domingo")
+    elif dia ="2":
+          print("Sabado")
+    elif dia ="3":
+              print("Segunda")
+    elif dia ="4":
+              print("terça")
+    elif dia ="5":
+              print("quarta")
+    elif dia ="6":
+              print("quinta")
+    elif dia ="7":
+              print("Sexta")
+      else:
+              print("inválido")
+              
+'''Usuario vai digitar a idade se ele for de 0 até 10 imprime na tela criança de 11 até 17 imprime adoslecente de 18 até 25  impirme jovem de 26 até 50 impirme adulto 
+                   de 51 até 70 impirme idoso de 71 pra 120 imprime terceira idade qualquer coisa fora disso imprime inválido'''
+                   
+                   idade=int(input("Digite sua idade \n>"))
+                    elif idade ="0>10":
+                    elif idade ="10<0":
+                    print("Usuário ainda é criança")
+                    elif idade ="11>17":
+                    elif idade ="17<11":
+                    print("Usuário é um adoslecente")
+                    elif idade ="18>25":
+                    elif idade ="25<18":
+                     print("Usuário é um jovem")
+                    elif idade="26>50":
+                    elif idade="50<26":
+                    print("Usuário é adulto")
+                    elif idade="50>70":
+                    elif idade="70<50":
+                    print("Usuário é experiente")
+                    elif idade="71>120":
+                    elif idade="71<120":
+                    print("Usuário é terceira idade")
+                    else:
+                        print("Inválido")
+                        
+   '''Usuario vai digitar a idade se ele for de 0 até 10 imprime na tela criança de 11 até 17 imprime adoslecente de 18 até 25  impirme jovem de 26 até 50 impirme adulto 
+                   de 51 até 70 impirme idoso de 71 pra 120 imprime terceira idade qualquer coisa fora disso imprime inválido'''
+                   
+                   
+     idade=int(input("Digite uma idade \n>"))
+     if (idade>=0) and (idade<=10);
+        print("Usuário ainda é criança")
+    elif(idade>=11) and (idade<=17)
+        print("Usuário ainda é adoslecente")
+    elif(idade>=18) and (idade<=25)
+       print("Usuário ainda é jovem")
+  elif(idade>=26) and (idade<=50)
+       print("Usuário ainda é adulto")
+   elif(idade>=51) and (idade<=70)
+          print("Usuário ainda é experiente")
+  elif(idade>=71) and (idade<=120)
+        print("Usuário é terceira idade")
+     else:
+       print("Inválido")
+       
+ 
+ 
+
+
+    # Achando o maior número
+
+
+'''    if (segundo > maior):
+        maior = segundo
+    if (terceiro > maior):
+        maior = terceiro
+
+    print('Maior: ',maior)
+
+    # Achando o menor número
+    menor = primeiro
+
+    if (segundo < menor):
+        menor = segundo
+    if (terceiro < menor):
+        menor = terceiro
+
+    print('Menor: ',menor)'''
+    
+
+           
+           
+           
+           
+    ####################################################################
+    
+    
+    n1=int(input("Digite um número \n>"))
+    n2=int(input("Digite o segundo número \n>"))
+    n3=int(input("Digite o terceiro número \n>"))
+    
+    if(n1>n2)and(n1>n3)
+    print("n1 é o maior")
+     if(n2>n1)and(n2>n3)
+     print("n2 é o maior")
+      if(n3>n1)and(n3>n2)
+      print("n3 é o maior")
+      
+      #### >>>>>>>>>>>>>>>>>>>>>>>>>>>ERRADO###################
+      
+      
+      
+                        
+                        
+    
+
+
