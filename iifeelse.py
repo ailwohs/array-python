ddd3ff9d59a4b99ac70800c45473a32bd523f521
Julia Leithold <juliaavancisoares@gmail.com>
2024-02-24 21:12:33 -0300

    adicionando python

---
+++ iifeelse.py
@@ -0,0 +1,73 @@
+#APRENDENDO IF E ELSE AULA DIA 14/08 
+
+
+'''n=int(input("Digite número: \t"))
+
+if (n>100):
+    print("A divisão desse número é:" n/4,)
+else:
+    print("O número elevado ao quadrado é:" n**4)
+print(50"#")
+print("ATÉ MAIS")'''
+
+
+'''n = int(input("Digite número: \t"))
+
+if n > 100:
+    print("A divisão desse número é:", n / 4)
+else:
+    print("O número elevado ao quadrado é:", n ** 2)
+#print("#"50)
+print("ATÉ MAIS")'''
+
+
+
+#programa que pede ao usuario dois números verifique qual dos dois é maior e imprima 
+
+
+'''a=int(input("Digite o primeiro número: \t"))
+b=int(input("Digite o segundo número: \t"))
+
+if(a>b):
+ print("O maior número é:", a)
+ #print("O maior número é %d"(a))
+else:
+    print("O maior número é:", b)'''
+ 
+ 
+ 
+#programa para o usuario digitar um número e depois imprimir se é par ou impar 
+
+'''num=int(input("Digite um número: \t"))
+
+if(num %2==0):
+    print("O número:", num , "é par")
+else:
+    print("O número:", num , "é ímpar")'''
+
+    
+
+
+
+#programa pede três números para o usuário imprime os números em ordem crescente do menor para o maior 
+
+'''a=int(input("Digite o primeiro número: \t"))
+b=int(input("Digite o segundo número: \t"))
+c=int(input("Digite o terceiro número: \t"))
+
+if a <= b and a <= c:
+    if b <= c:
+        print(a, b, c)
+    else:
+        print(a, c, b)
+elif b <= a and b <= c:
+    if a <= c:
+        print(b, a, c)
+    else:
+        print(b, c, a)
+else:
+    if a <= b:
+        print(c, a, b)
+    else:
+        print(c, b, a)'''
+
